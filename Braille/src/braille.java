b204897d2026dcebc502a252f54e7eb1d838df0a
Mbukutshe <kmbukuts@student.wethinkcode.co.za>
2020-02-04 00:46:49 +0200

    Add files via upload

---
+++ Braille/src/braille.java
@@ -0,0 +1,405 @@
+import java.util.Scanner;
+public class braille {
+
+	public static void main(String[] args) {
+		char[][] a = new char[3][2];
+		a[0][0] = '#';
+		a[0][1] = '*';
+		a[1][0] = '*';
+		a[1][1] = '*';
+		a[2][0] = '*';
+		a[2][1] = '*';
+		
+		char[][] b = new char[3][2];
+		b[0][0] = '#';
+		b[0][1] = '*';
+		b[1][0] = '#';
+		b[1][1] = '*';
+		b[2][0] = '*';
+		b[2][1] = '*';
+		
+		char[][] c = new char[3][2];
+		c[0][0] = '#';
+		c[0][1] = '#';
+		c[1][0] = '*';
+		c[1][1] = '*';
+		c[2][0] = '*';
+		c[2][1] = '*';
+		
+		char[][] d = new char[3][2];
+		d[0][0] = '#';
+		d[0][1] = '#';
+		d[1][0] = '*';
+		d[1][1] = '#';
+		d[2][0] = '*';
+		d[2][1] = '*';
+		
+		char[][] e = new char[3][2];
+		e[0][0] = '#';
+		e[0][1] = '*';
+		e[1][0] = '*';
+		e[1][1] = '#';
+		e[2][0] = '*';
+		e[2][1] = '*';
+		
+		char[][] f = new char[3][2];
+		f[0][0] = '#';
+		f[0][1] = '#';
+		f[1][0] = '#';
+		f[1][1] = '*';
+		f[2][0] = '*';
+		f[2][1] = '*';
+		
+		char[][] g = new char[3][2];
+		g[0][0] = '#';
+		g[0][1] = '#';
+		g[1][0] = '#';
+		g[1][1] = '#';
+		g[2][0] = '*';
+		g[2][1] = '*';
+		
+		char[][] h = new char[3][2];
+		h[0][0] = '#';
+		h[0][1] = '*';
+		h[1][0] = '#';
+		h[1][1] = '#';
+		h[2][0] = '*';
+		h[2][1] = '*';
+		
+		char[][] il = new char[3][2];
+		il[0][0] = '*';
+		il[0][1] = '#';
+		il[1][0] = '#';
+		il[1][1] = '*';
+		il[2][0] = '*';
+		il[2][1] = '*';
+		
+		char[][] jl = new char[3][2];
+		jl[0][0] = '*';
+		jl[0][1] = '#';
+		jl[1][0] = '#';
+		jl[1][1] = '#';
+		jl[2][0] = '*';
+		jl[2][1] = '*';
+		
+		char [][] k = new char[3][2];
+		k[0][0] = '#';
+		k[0][1] = '*';
+		k[1][0] = '*';
+		k[1][1] = '*';
+		k[2][0] = '#';
+		k[2][1] = '*';
+		
+		char [][] l = new char[3][2];
+		l[0][0] = '#';
+		l[0][1] = '*';
+		l[1][0] = '#';
+		l[1][1] = '*';
+		l[2][0] = '#';
+		l[2][1] = '*';
+		
+		char [][] m = new char[3][2];
+		m[0][0] = '#';
+		m[0][1] = '#';
+		m[1][0] = '*';
+		m[1][1] = '*';
+		m[2][0] = '#';
+		m[2][1] = '*';
+		
+		char [][] n = new char[3][2];
+		n[0][0] = '#';
+		n[0][1] = '#';
+		n[1][0] = '*';
+		n[1][1] = '#';
+		n[2][0] = '#';
+		n[2][1] = '*';
+		
+		char [][] o = new char[3][2];
+		o[0][0] = '#';
+		o[0][1] = '*';
+		o[1][0] = '*';
+		o[1][1] = '#';
+		o[2][0] = '#';
+		o[2][1] = '*';
+		
+		char [][] p = new char[3][2];
+		p[0][0] = '#';
+		p[0][1] = '#';
+		p[1][0] = '#';
+		p[1][1] = '*';
+		p[2][0] = '#';
+		p[2][1] = '*';
+		
+		char [][] q = new char[3][2];
+		q[0][0] = '#';
+		q[0][1] = '#';
+		q[1][0] = '#';
+		q[1][1] = '#';
+		q[2][0] = '#';
+		q[2][1] = '*';
+		
+		char [][] r = new char[3][2];
+		r[0][0] = '#';
+		r[0][1] = '*';
+		r[1][0] = '#';
+		r[1][1] = '#';
+		r[2][0] = '#';
+		r[2][1] = '*';
+		
+		char [][] s = new char[3][2];
+		s[0][0] = '*';
+		s[0][1] = '#';
+		s[1][0] = '#';
+		s[1][1] = '*';
+		s[2][0] = '#';
+		s[2][1] = '*';
+		
+		char [][] t = new char[3][2];
+		t[0][0] = '*';
+		t[0][1] = '#';
+		t[1][0] = '#';
+		t[1][1] = '#';
+		t[2][0] = '#';
+		t[2][1] = '*';
+		
+		char [][] u = new char[3][2];
+		u[0][0] = '#';
+		u[0][1] = '*';
+		u[1][0] = '*';
+		u[1][1] = '*';
+		u[2][0] = '#';
+		u[2][1] = '#';
+		
+		char [][] v = new char[3][2];
+		v[0][0] = '#';
+		v[0][1] = '*';
+		v[1][0] = '#';
+		v[1][1] = '*';
+		v[2][0] = '#';
+		v[2][1] = '#';
+		
+		char [][] w = new char[3][2];
+		w[0][0] = '*';
+		w[0][1] = '#';
+		w[1][0] = '#';
+		w[1][1] = '#';
+		w[2][0] = '*';
+		w[2][1] = '#';
+		
+		char [][] x = new char[3][2];
+		x[0][0] = '#';
+		x[0][1] = '#';
+		x[1][0] = '*';
+		x[1][1] = '*';
+		x[2][0] = '#';
+		x[2][1] = '#';
+		
+		char [][] y = new char[3][2];
+		y[0][0] = '#';
+		y[0][1] = '#';
+		y[1][0] = '*';
+		y[1][1] = '#';
+		y[2][0] = '#';
+		y[2][1] = '#';
+		
+		char [][] z = new char[3][2];
+		z[0][0] = '#';
+		z[0][1] = '*';
+		z[1][0] = '*';
+		z[1][1] = '#';
+		z[2][0] = '#';
+		z[2][1] = '#';
+		
+		String text;
+		int len = 0;
+		Scanner kb = new Scanner(System.in);
+		System.out.println("Please Enter Characters in this format A - Z:");
+		text = kb.nextLine();
+		len = text.length();
+        for (int i = 0; i < 3; i++) {
+        	if (text == "A")
+        	{
+	            for (int j = 0; j < 2; j++) {
+	            	System.out.print(a[i][j]);
+	            }
+        	}
+        	if (text == "B")
+        	{
+	            System.out.print(" ");
+	            for (int j = 0; j < 2; j++) {
+	            	System.out.print(b[i][j]);
+	            }
+        	}
+        	if (text == "C")
+        	{
+	            System.out.print(" ");
+	            for (int j = 0; j < 2; j++) {
+	            	System.out.print(c[i][j]);
+	            }
+        	}
+        	if (text == "D")
+        	{
+	            System.out.print(" ");
+	            for (int j = 0; j < 2; j++) {
+	            	System.out.print(d[i][j]);
+	            }
+        	}
+        	if (text == "E")
+        	{
+	            System.out.print(" ");
+	            for (int j = 0; j < 2; j++) {
+	            	System.out.print(e[i][j]);
+	            }
+        	}
+        	if (text == "F")
+        	{
+	            System.out.print(" ");
+	            for (int j = 0; j < 2; j++) {
+	            	System.out.print(f[i][j]);
+	            }
+        	}
+        	if (text == "G")
+        	{
+	            System.out.print(" ");
+	            for (int j = 0; j < 2; j++) {
+	            	System.out.print(g[i][j]);
+	            }
+        	}
+        	if (text == "H")
+        	{
+	            System.out.print(" ");
+	            for (int j = 0; j < 2; j++) {
+	            	System.out.print(h[i][j]);
+	            }
+        	}
+        	if (text == "I")
+        	{
+	            System.out.print(" ");
+	            for (int j = 0; j < 2; j++) {
+	            	System.out.print(il[i][j]);
+	            }
+        	}
+        	if (text == "J")
+        	{
+	            System.out.print(" ");
+	            for (int j = 0; j < 2; j++) {
+	            	System.out.print(jl[i][j]);
+	            }
+        	}
+        	if (text == "K")
+        	{
+	            System.out.print(" ");
+	            for (int j = 0; j < 2; j++) {
+	            	System.out.print(k[i][j]);
+	            }
+        	}
+        	if (text == "L")
+        	{
+	            System.out.print(" ");
+	            for (int j = 0; j < 2; j++) {
+	            	System.out.print(l[i][j]);
+	            }
+        	}
+        	if (text == "M")
+        	{
+	            System.out.print(" ");
+	            for (int j = 0; j < 2; j++) {
+	            	System.out.print(m[i][j]);
+	            }
+        	}
+        	if (text == "N")
+        	{
+	            System.out.print(" ");
+	            for (int j = 0; j < 2; j++) {
+	            	System.out.print(n[i][j]);
+	            }
+        	}
+        	if (text == "O")
+        	{
+	            System.out.print(" ");
+	            for (int j = 0; j < 2; j++) {
+	            	System.out.print(o[i][j]);
+	            }
+        	}
+        	if (text == "P")
+        	{
+	            System.out.print(" ");
+	            for (int j = 0; j < 2; j++) {
+	            	System.out.print(p[i][j]);
+	            }
+        	}
+        	if (text == "Q")
+        	{
+	            System.out.print(" ");
+	            for (int j = 0; j < 2; j++) {
+	            	System.out.print(q[i][j]);
+	            }
+        	}
+        	if (text == "R")
+        	{
+	            System.out.print(" ");
+	            for (int j = 0; j < 2; j++) {
+	            	System.out.print(r[i][j]);
+	            }
+        	}
+        	if (text == "S")
+        	{
+	            System.out.print(" ");
+	            for (int j = 0; j < 2; j++) {
+	            	System.out.print(s[i][j]);
+	            }
+        	}
+        	if (text == "T")
+        	{
+	            System.out.print(" ");
+	            for (int j = 0; j < 2; j++) {
+	            	System.out.print(t[i][j]);
+	            }
+        	}
+        	if (text == "U")
+        	{
+	            System.out.print(" ");
+	            for (int j = 0; j < 2; j++) {
+	            	System.out.print(u[i][j]);
+	            }
+        	}
+        	if (text == "V")
+        	{
+	            System.out.print(" ");
+	            for (int j = 0; j < 2; j++) {
+	            	System.out.print(v[i][j]);
+	            }
+        	}
+        	if (text == "W")
+        	{
+	            System.out.print(" ");
+	            for (int j = 0; j < 2; j++) {
+	            	System.out.print(w[i][j]);
+	            }
+        	}
+        	if (text == "X")
+        	{
+	            System.out.print(" ");
+	            for (int j = 0; j < 2; j++) {
+	            	System.out.print(x[i][j]);
+	            }
+        	}
+        	if (text == "Y")
+        	{
+	            System.out.print(" ");
+	            for (int j = 0; j < 2; j++) {
+	            	System.out.print(y[i][j]);
+	            }
+        	}
+        	if (text == "Z")
+        	{
+	            System.out.print(" ");
+	            for (int j = 0; j < 2; j++) {
+	            	System.out.print(z[i][j]);
+	            }
+        	}
+            System.out.println();
+        }
+	}
+	
+}
